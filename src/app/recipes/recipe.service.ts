0710b7abfa62e602535ee665f9738e801325c881
Pierre Le Count <pmlc@iinet.net.au>
2018-12-18 23:05:00 +0000

    Add ingredieant to shopping list from recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -2,13 +2,14 @@ import { EventEmitter, Injectable } from '@angular/core';
 
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
-  
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-      new Recipe(
+    new Recipe(
       'Tasty Schnitzel',
       'A super-tasty Schnitzel - just awesome!',
       'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
@@ -24,10 +25,14 @@ export class RecipeService {
         new Ingredient('Meat', 1)
       ])
   ];
-    
+
+  constructor(private slService: ShoppingListService) {}
+
   getRecipes() {
     return this.recipes.slice();
-  }    
-    
+  }
 
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
 }
