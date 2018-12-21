466cccad26064b9d7dccd172b6c4e71800fabd94
Pierre Le Count <pmlc@iinet.net.au>
2018-12-21 22:50:51 +0000

    Edit recipes - new module and routes added = 160

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,25 +15,26 @@ import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent, 
-    HeaderComponent, 
-    RecipesComponent, 
-    RecipeListComponent, 
-    RecipeDetailComponent, 
-    RecipeItemComponent, 
-    ShoppingListComponent, 
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
-    RecipeStartComponent
+    RecipeStartComponent,
+    RecipeEditComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule,
+    HttpClientModule, 
     AppRoutingModule
   ],
   providers: [ShoppingListService],
