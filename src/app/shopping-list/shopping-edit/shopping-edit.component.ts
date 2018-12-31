73f64388bad6cc36f1dbc234b58bb0bba55be8ed
Pierre Le Count <pmlc@iinet.net.au>
2018-12-31 00:44:41 +0000

    Ingredients - intial work - 206

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,8 @@
 import {
   Component,
-  OnInit,
-  ElementRef,
-  ViewChild,
-  Output
+  OnInit
 } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
@@ -15,18 +13,20 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput') nameInputRef: ElementRef;
-  @ViewChild('amountInput') amountInputRef: ElementRef;
 
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
   }
 
-  onAddItem() {
-    const ingName = this.nameInputRef.nativeElement.value;
-    const ingAmount = this.amountInputRef.nativeElement.value;
-    const newIngredient = new Ingredient(ingName, ingAmount);
+  onAddItem(form: NgForm) {
+    const value = form.value;
+    const newIngredient = new Ingredient(value.name, value.amount);
+    console.log("  ");
+    console.log("  ");
+    console.log(value.name);
+    console.log("  ");
+    console.log("  ");
     this.slService.addIngredient(newIngredient);
   }
 
