f05382cbe6483fa171f8e8f219c856fe863be57a
Pierre Le Count <pmlc@iinet.net.au>
2019-01-01 02:43:08 +0000

    Adit.insert finished

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -39,7 +39,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy  {
       );
   }
   
-  onAddItem(form: NgForm) {
+  onSubmit(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
     if (this.editMode) {
@@ -47,8 +47,9 @@ export class ShoppingEditComponent implements OnInit, OnDestroy  {
     } else {
       this.slService.addIngredient(newIngredient);
     }
+    this.editMode = false;
+    form.reset();
   }
-  
   
   ngOnDestroy() {
     this.subscription.unsubscribe();
